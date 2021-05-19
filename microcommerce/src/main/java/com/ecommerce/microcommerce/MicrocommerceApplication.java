19483d18c7ddb35dff6708eee28a4aa43cf64ec8
bchauvet <bchauvet@NXQLX140.nx-domain.lan>
2021-05-19 10:41:00 +0200

    a

--- microcommerce/src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
+++ microcommerce/src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicrocommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicrocommerceApplication.class, args);
+		SpringApplication.run(MicrocommerceApplication.class, args); 
 	}
 
 }
